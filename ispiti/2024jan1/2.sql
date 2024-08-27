edce10baf14d15cc73c135b52d6670adf49feab5
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2024-08-27 13:46:05 -0700

    Update 2.sql

--- ispiti/2024jan1/2.sql
+++ ispiti/2024jan1/2.sql
@@ -1,18 +1,16 @@
-select d.ime || ' ' || d.prezime "Ime i prezime",
+select d.ime || ' ' || d.prezime "Ime i prezime", 
 	case
-		when d.pol = 'm' then 'Student je jedini iz mesta ' || d.mestorodjenja ||
-								' sa upisanom ' || ug.skgodina || ' skolsku godinu.'
-		else 'Studentkinja je jedina iz mesta ' || d.mestorodjenja ||
-								' sa upisanom ' || ug.skgodina || ' skolsku godinu.'
+		when d.pol = 'm' then 'Student je jedini iz ' || d.mestorodjenja || ' upisao ' || ug.skgodina || ' sk godinu'
+		else 'Studentkinja je jedina iz ' || d.mestorodjenja || ' upisala ' || ug.skgodina || ' sk godinu'
 	end "Komentar",
-	DAYS(CURRENT_DATE) - DAYS(d.datupisa) "Proteklo dana"
-from da.dosije d join da.upisgodine ug
-		on d.indeks = ug.indeks
+	days(current_date) - days(d.datupisa) "Proteklo dana"
+from da.dosije d join  da.upisgodine ug
+	on d.indeks = ug.indeks
 where not exists(
 	select d1.indeks
 	from da.dosije d1 join da.upisgodine ug1
 		on d1.indeks = ug1.indeks
-	where d1.indeks < d.indeks and d1.mestorodjenja = d.mestorodjenja and ug1.skgodina = ug.skgodina
+	where d1.indeks <> d.indeks and d1.mestorodjenja = d.mestorodjenja and ug1.skgodina = ug.skgodina
 )
 and 5 > (
 	select count(*)
@@ -20,3 +18,4 @@ and 5 > (
 	where i.indeks = d.indeks and ocena > 5 and i.status = 'o'
 	group by i.indeks
 )
+order by "Proteklo dana" desc
