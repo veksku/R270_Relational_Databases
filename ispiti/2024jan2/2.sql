5b8b453d64f5a635038948f8475fdfdd1afb584e
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2024-08-27 11:58:56 -0700

    Update 2.sql

--- ispiti/2024jan2/2.sql
+++ ispiti/2024jan2/2.sql
@@ -1,29 +1,28 @@
-select distinct d.indeks, d.ime || ' ' || d.prezime "Ime i prezime", 
+select distinct d.indeks, d.ime || ' ' || d.prezime "Ime i prezime",
 	case
 		when d.idstatusa = 1 then sp.naziv
-		else p.naziv
-	end "Komentar", 
+		else p.naziv || ' ' || isnull(i.ocena, 5)
+	end "Komentar",
 	case
-		when d.idstatusa = 1 then DAYNAME(d.datupisa)
-		else DAYNAME(i.datpolaganja)
+		when d.idstatusa = 1 then dayname(d.datupisa)
+		else dayname(i.datpolaganja)
 	end "Naziv dana"
-from da.dosije d join da.upisankurs uk
-		on uk.indeks = d.indeks
+from da.dosije d join da.ispit i
+		on d.indeks = i.indeks
 	join da.predmet p
-		on p.id = uk.idpredmeta
-	join da.ispit i
-		on i.idpredmeta = p.id
+		on p.id = i.idpredmeta
 	join da.studijskiprogram sp
 		on sp.id = d.idprograma
-where d.idstatusa = 1 and 4 < (
-	select count(*)
-	from da.upisankurs uk 
-	where uk.indeks = d.indeks and uk.idpredmeta = p.id
-	group by idpredmeta
-)
-or 5 < (
-	select count(*)
-	from da.upisankurs uk 
-	where uk.indeks = d.indeks and uk.idpredmeta = p.id
-	group by idpredmeta
-)
+where d.idstatusa = 1
+	and 4 < (
+		select count(*)
+		from da.upisankurs uk
+		where uk.indeks = d.indeks and uk.idpredmeta = p.id
+		group by idpredmeta
+	)
+	or 5 = (
+		select count(*)
+		from da.upisankurs uk
+		where uk.indeks = d.indeks and uk.idpredmeta = p.id
+		group by idpredmeta
+	)
