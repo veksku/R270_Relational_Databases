4debaccde506d8618cde2f47ccfbaf62e93b55f3
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2024-08-27 13:27:58 -0700

    Update 1.sql

--- ispiti/2024jan1/1.sql
+++ ispiti/2024jan1/1.sql
@@ -1,20 +1,11 @@
-select d.indeks, d.ime, d.prezime, d.mestorodjenja, 
-	case
-		when decimal(avg(ocena + 0.0), 4, 2) is null then 0.00
-		else decimal(avg(ocena + 0.0), 4, 2)
-	end
-	prosek,
-	case
-		when d.mestorodjenja like '%Beograd%' then
-			 d.ime || ' ' || d.prezime || ' (' || replace(d.mestorodjenja, 'Beograd', 'Bg') || ')'
-		else d.ime || ' ' || d.prezime || ' (' || d.mestorodjenja || ')'
-	end
-	kod
+select d.indeks, d.ime, d.prezime, d.mestorodjenja, decimal(avg(i.ocena+0.0), 4, 2) "Prosek",
+	d.ime || ' ' || d.prezime || ' (' || replace(d.mestorodjenja, 'Beograd', 'Bg') || ')'"Kod"
 from da.dosije d join da.ispit i
-	on d.indeks = i.indeks
-where 2 >= (
-	select count(status)
-	from da.ispit i
-	where i.indeks = d.indeks and status='x'
-)
+		on d.indeks = i.indeks
+where i.ocena > 5 and i.status = 'o'
+	and 2 >= (
+		select count(*)
+		from da.ispit i1
+		where i1.indeks = d.indeks and status = 'x'
+	)
 group by d.indeks, d.ime, d.prezime, d.mestorodjenja
