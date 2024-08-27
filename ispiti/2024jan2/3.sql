a840a30e11adeb7368dc9f0013abcb31cfe645e9
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2024-08-27 12:59:38 -0700

    Update 3.sql

--- ispiti/2024jan2/3.sql
+++ ispiti/2024jan2/3.sql
@@ -1,56 +1,55 @@
 --a
 
-create table smerovi (
+create table smerovi(
 	id integer primary key not null,
 	naziv varchar(100),
 	nivo varchar(10),
 	studenti integer,
 	prosek double,
-	constraint vrednost_nivo check (nivo in ('osnovne','master','doktorske'))
+	constraint vrednost_nivo check (nivo in ('osnovne', 'master', 'doktorske'))
 );
-
 
 --b
 
-create function to_cm(broj integer, jed_mere varchar(1))
+create function to_cm(broj integer, jmere varchar(1))
 returns integer
 return
 	case
-		when jed_mere = 'd' then broj*10
-		when jed_mere = 'm' then broj*100
-		when jed_mere = 'k' then broj*1000
+		when jmere = 'd' or jmere = 'D' then broj*10
+		when jmere = 'm' or jmere = 'M' then broj*100
+		when jmere = 'k' or jmere = 'K' then broj*1000
 		else -1
 	end;
-	
 	
 --c
 
 insert into smerovi(id, naziv, studenti)
 select sp.id, sp.naziv, count(*) br_studenata
 from da.studijskiprogram sp join da.dosije d
-	on d.idstatusa = sp.idnivoa and sp.idnivoa = 1
-group by sp.id, sp.naziv
-
+	on d.idprograma = sp.id
+where sp.idnivoa = 1
+group by sp.id, sp.naziv;
 
 --d
 
-
-
+insert into smerovi(id, naziv, studenti, prosek)
+select sp.id, sp.naziv, count(*) br_studenata, decimal(avg(ocena+0.0), 4, 2)
+from da.studijskiprogram sp join da.dosije d
+		on d.idprograma = sp.id
+	join da.ispit i
+		on d.indeks = i.indeks and i.ocena > 5 and i.status = 'o'
+where sp.idnivoa = 2  or sp.idnivoa = 3
+group by sp.id, sp.naziv;
+--ovo samo dodaje redove za master i doktorske, nmp kako da se i azuriraju vrednosti bez update
 
 --e
 
 delete from smerovi
-where (studenti<1000) and (prosek>9.0);
+where studenti < 100 and prosek > 9.0;
 
+--pomocne f-je
 
-
-
-
-
-
-
-
-
-
-
-
+select *
+from smerovi;
+drop table smerovi;
+drop function to_cm;
